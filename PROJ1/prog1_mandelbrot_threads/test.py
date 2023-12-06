b1797ad3e3c076d75b95775db76ce45b314ee39d
BUAA-TYZ <2506741037@qq.com>
2023-12-06 16:43:53 +0800

    FINISH PROJ1 PART1 | Add a script which can draw the speedup picture

---
+++ PROJ1/prog1_mandelbrot_threads/test.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import os
+
+os.popen("make mandelbrot")
+thread_num = [i for i in range(2, 17)]
+speedup_res = []
+for i in thread_num:
+  command = "./mandelbrot -t " + str(i)
+  myoutput = os.popen(command).read()
+  rate_begin = myoutput.index('(')
+  rate = float(myoutput[rate_begin+1:rate_begin+5])
+  speedup_res.append(rate)
+fig = plt.figure()
+plt.plot(thread_num, speedup_res)
+plt.savefig("rate.png")
+plt.show()
